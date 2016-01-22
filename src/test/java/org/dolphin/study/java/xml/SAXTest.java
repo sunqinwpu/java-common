e6d2c5a2d5dae40a0d19c707a2ce57b0a259183e
三醒 <qi.sq@alibaba-inc.com>
2016-01-22 21:22:33 +0800

    add SAXTest

--- src/test/java/org/dolphin/study/java/xml/SAXTest.java
+++ src/test/java/org/dolphin/study/java/xml/SAXTest.java
@@ -6,8 +6,12 @@ package org.dolphin.study.java.xml;
 
 import java.io.InputStream;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  *
@@ -22,8 +26,23 @@ public class SAXTest {
      * 测试SAX解析XML
      */
     public void testSaxParse() {
-        InputStream inputStream = DomTest.class.getClassLoader().getResourceAsStream(
-            "xml/books.xml");
-        
+        try {
+            InputStream inputStream = DomTest.class.getClassLoader().getResourceAsStream(
+                "xml/books.xml");
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(inputStream, new BooksHandler());
+        } catch (Exception e) {
+            logger.error("parse exception", e);
+        }
     }
+}
+
+/**
+ *
+ * @author sunqi
+ * @version $Id: SAXTest.java, v 0.1 2016年1月19日 下午3:52:36 sunqi Exp $
+ */
+class BooksHandler extends DefaultHandler {
+
 }
